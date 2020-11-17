9ee3e1d2f9534e6431dc0f639d3ab0a21516ca03
ViolenceSuppressor <Harderus@yandex.ru>
2020-11-17 14:33:21 +0300

    add 24(c++)

---
+++ Practice/24/C++/24/24.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <json.hpp>
+#include <fstream>
+#include <iomanip>
+
+using json = nlohmann::json;
+
+int main()
+{
+	json jinput;
+	json joutput;
+	std::ifstream reader("in.json");
+	reader >> jinput;
+	reader.close();
+
+
+	for (auto& object : jinput) 
+	{
+		if (object["completed"] == true)
+		{
+			bool contains = false;
+			for (auto& entry : joutput)
+			{
+				if (entry["userId"] == object["userId"])
+				{
+					contains = true;
+					entry["task_completed"] = entry["task_completed"] + 1;//из-за этой строки угробил полчаса
+					break;
+				}
+			}
+			if (!contains)
+			{
+				joutput.push_back({ {"userId", object["userId"]}, {"task_completed",1} });
+			}
+		}
+	}
+
+	std::ofstream writer("out.json");
+	writer << std::setw(4) << joutput<<std::endl;
+	writer.close();
+}
